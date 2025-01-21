5a806bdbbd160b5ec0975f3681f23b93014921ad
Deily13 <quinceyton.17@gamil.com>
2025-01-21 06:28:01 -0500

    Entidad producto definida

--- src/main/java/com/example/Finance_crud_tool/entity/Product.java
+++ src/main/java/com/example/Finance_crud_tool/entity/Product.java
@@ -1,4 +1,46 @@
 package com.example.Finance_crud_tool.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Table(name = "produtc")
+@Entity(name = "Product")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type", nullable = false)
+    private AccountType accountType;
+
+    public enum AccountType {
+        SAVINGS,
+        CURRENT
+    }
+
+    private int account_number;
+
+    private String status;
+
+    private BigDecimal balance;
+
+    private BigDecimal exempt_GMF;
+
+    private LocalDateTime creation_date = LocalDateTime.now();
+
+    private LocalDateTime update_date = LocalDateTime.now();
+
 }
