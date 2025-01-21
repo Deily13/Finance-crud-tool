23d2fafb98d2ae7e6f94611ead83b27e3c82c8dd
Deily13 <quinceyton.17@gamil.com>
2025-01-20 21:41:12 -0500

    primer commit

---
+++ src/main/java/com/example/Finance_crud_tool/FinanceCrudToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.Finance_crud_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCrudToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCrudToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Finance_crud_tool/FinanceCrudToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Finance_crud_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCrudToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
